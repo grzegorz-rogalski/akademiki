4b902edf1757031a0c8d18b96c9080d41e30bc40
marek-szymanski <marekszymanski@live.com>
2015-02-28 14:19:22 +0100

    Możliwość rejestracji (zapis do bazy jeśli login nie był użyty) i logowania dla zapisanych userów - w systemAkademika2

--- projekt/systemAkademika2/src/java/projekt/osoba/baza/OsobaSes.java
+++ projekt/systemAkademika2/src/java/projekt/osoba/baza/OsobaSes.java
@@ -7,10 +7,17 @@
 package projekt.osoba.baza;
 
 import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
+import javax.persistence.criteria.Root;
+import projekt.messages.messageKontroler;
 
 
 /**
@@ -22,22 +29,44 @@ public class OsobaSes {
 
     @PersistenceContext
     private EntityManager manager;
+    private BazaPearson temp = new BazaPearson();
+    messageKontroler message;
     
-    public void createOsoba(BazaPearson osoba) {
-        if(osoba.getId()==0||osoba.getId()==1)
+    public void createOsoba(BazaPearson osoba) 
+    {
+        message= new messageKontroler();
+        List<BazaPearson> list = findLog(osoba.getLogin());
+        if(list.isEmpty())
         {
-            osoba.setStatus("Admin");
+            message.rejestracja();
+            manager.persist(osoba);
         }
         else
-        {
-            osoba.setStatus("BRAK");
-        }
-        manager.persist(osoba);
+            message.rejestracjaError();
     }
     
     public List<BazaPearson> allPeople() {
         CriteriaQuery cq = manager.getCriteriaBuilder().createQuery();
         cq.select(cq.from(BazaPearson.class));
         return manager.createQuery(cq).getResultList();
+    }
+    
+    public BazaPearson findID(int id)
+    {
+        temp=manager.find(BazaPearson.class, id);
+        return temp;
+    }
+    
+    public List<BazaPearson> findLog(String login)
+    {
+        // Select the employees and the mailing addresses that have the same address.
+        CriteriaBuilder cb = manager.getCriteriaBuilder();
+        CriteriaQuery cq = cb.createQuery();
+        Root e = cq.from(BazaPearson.class);
+        cq.multiselect(e);
+        cq.where(cb.equal(e.get("login"), login));
+        Query query = manager.createQuery(cq);
+        List<BazaPearson> result = query.getResultList();
+        return result;
     }
 }
--- projekt/systemAkademika2/src/java/projetk/kontroler/kontroler.java
+++ projekt/systemAkademika2/src/java/projetk/kontroler/kontroler.java
@@ -9,8 +9,11 @@ package projetk.kontroler;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import org.primefaces.context.RequestContext;
+import projekt.messages.messageKontroler;
 import projekt.osoba.baza.BazaPearson;
 import projekt.osoba.baza.OsobaSes;
 
@@ -24,30 +27,76 @@ public class kontroler {
     //Zmienne !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     @EJB
     private OsobaSes request;
+    private messageKontroler message;
     
     private BazaPearson osoba;
     private List<BazaPearson> listaOsob = new ArrayList<>();
     //Zmienne !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     
     public kontroler() {
+        this.message = new messageKontroler();
         osoba=new BazaPearson();
     }
     
-    //Funkcje obslugi zdarzen ###########################
+    //Funkcje obslugi zdarzen bazy ###########################
+    
+    //dodaje osobe do bazy
     public String dodajOsobe()
     {
         request.createOsoba(osoba);
         return null;
     }
     
+    //tworzy liste wszystkich osob z bazy
     public String wszystkieOsoby()
     {
         listaOsob=request.allPeople();
         return null;
     }
     
+    public String osobaPoId(int id)
+    {
+        osoba=request.findID(id);
+        return null;
+    }
     
-    //Funkcje obslugi zdarzen ###########################
+    public BazaPearson osobaPoLogin(String log)
+    {
+        listaOsob=request.findLog(log);
+        if(listaOsob.isEmpty())
+        {
+            message.logwanieError();
+            return null;
+        }
+        BazaPearson temp=listaOsob.get(0);
+        return temp;
+    }
+    
+    //Funkcje obslugi zdarzen bazy ###########################
+    
+    
+    //funkcje przejscia &&&&&&&&&&&&&&&&&&&&&&&&
+    
+    
+    
+    public String logowanie()
+    {
+        BazaPearson temp=osobaPoLogin(osoba.getLogin());
+        if (temp!=null&&temp.getLogin().equals(osoba.getLogin())&&temp.getHaslo().equals(osoba.getHaslo()))
+        {
+            osoba=temp;
+            System.out.println("TAK!!!!!!!!!!!!!!!!!!!!  "+temp.getImie());
+            return "test.xhtml";
+        }
+        else
+        {
+            message.logwanieError();
+        }
+        return null;
+    }
+    
+    //funkcje przejscia &&&&&&&&&&&&&&&&&&&&&&&&
+    
     
     //GETERY SETERY @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     public BazaPearson getOsoba() {

504451a21794ea6b5566569a2d3febae1ebe1225
grzegorz-rogalski <grzegorz@Mac-mini-Grzegorz.local>
2015-03-17 19:36:45 +0100

    dodanie tabel rady i akaddemika

---
+++ projekt/systemAkademika2/src/java/projekt/akademik/baza/AkademikDatabase.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekt.akademik.baza;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author grzegorz
+ */
+@Entity
+public class AkademikDatabase implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private int peopleId;
+    private int councilId;
+    private int roomId;
+
+    public int getPeopleId() {
+        return peopleId;
+    }
+
+    public void setPeopleId(int peopleId) {
+        this.peopleId = peopleId;
+    }
+
+    public int getCouncilId() {
+        return councilId;
+    }
+
+    public void setCouncilId(int councilId) {
+        this.councilId = councilId;
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof AkademikDatabase)) {
+            return false;
+        }
+        AkademikDatabase other = (AkademikDatabase) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "projekt.akademik.baza.AkademikDatabase[ id=" + id + " ]";
+    }
+    
+}

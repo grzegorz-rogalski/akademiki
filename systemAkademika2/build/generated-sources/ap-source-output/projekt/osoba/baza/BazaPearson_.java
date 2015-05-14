c5fb2c7f6bc741da114abce0e3938061caf1260c
Marek-Szymanski <marekszymanski@MacBook-Pro-Marek.local>
2015-05-14 18:43:21 +0200

    test
    test

--- systemAkademika2/build/generated-sources/ap-source-output/projekt/osoba/baza/BazaPearson_.java
+++ systemAkademika2/build/generated-sources/ap-source-output/projekt/osoba/baza/BazaPearson_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-04-30T11:27:55")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-05-05T17:26:09")
 @StaticMetamodel(BazaPearson.class)
 public class BazaPearson_ { 
 
